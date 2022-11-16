8eea19bdfd39662a670aff81400427e77d309803
Bekir Kasan <53233822+Bskasan@users.noreply.github.com>
2022-11-16 18:10:43 +0200

    Player Speed Variable Added

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,25 +1,21 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    //SerializedField attribute to see our variable on Inspector,Unity.
+    [SerializeField] float _speed = 1;
 
     // Update is called once per frame
     void Update()
     {
         // A float value from -1 to 1 to control our player.
-        var horizontal = Input.GetAxis("Horizontal");
+        var horizontal = Input.GetAxis("Horizontal") * _speed;
+
         // Assigning to rigidbody component of our player, to reference, access and modify.
         var rigidbody2D = GetComponent<Rigidbody2D>();
+
         rigidbody2D.velocity = new Vector2(horizontal, rigidbody2D.velocity.y); 
         // Velocity : the speed at which someting happens or moves. Hiz.
-
 
     }
 }
