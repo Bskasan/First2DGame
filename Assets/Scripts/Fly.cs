61f4b1bf9442d7e94604dd7b3fe8788956f4a122
Bskasan <bekirkasan1453@gmail.com>
2022-11-17 21:35:33 +0200

    Added direction and speed to Fly

--- Assets/Scripts/Fly.cs
+++ Assets/Scripts/Fly.cs
@@ -4,22 +4,26 @@ using UnityEngine;
 
 public class Fly : MonoBehaviour
 {
-    private Vector3 _startingPosition;
-    private Vector2 _direction = Vector2.up;
+    private Vector2 _startingPosition;
+    [SerializeField] private Vector2 _direction = Vector2.up;
+    [SerializeField] private float _maxDistance = 2;
+    [SerializeField] private float _speed = 2;
 
     // Start is called before the first frame update
     void Start()
     {
         _startingPosition = transform.position;
+        Debug.Log(new Vector3(0,2,0).normalized);
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(_direction * Time.deltaTime);
+        transform.Translate(_direction.normalized * Time.deltaTime * _speed);
         var distance = Vector2.Distance(_startingPosition, transform.position);
-        if (distance >= 2)
+        if (distance >= _maxDistance)
         {
+            transform.position = _startingPosition + (_direction.normalized * _maxDistance);
             _direction *= -1;
         }
     }
