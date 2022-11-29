2073cc3247678c114a9c3a481a26b71dd5119231
Bskasan <b.kasan@yandex.com>
2022-11-29 14:42:39 +0200

    Slime moves left and kills the player

---
+++ Assets/Scripts/Slime.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slime : MonoBehaviour
+{
+    private Rigidbody2D _rigidbody2D;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _rigidbody2D.velocity = Vector2.left;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Player player = collision.collider.GetComponent<Player>();
+        if (player == null)
+            return;
+
+        player.ResetToStart();
+    }
+}
