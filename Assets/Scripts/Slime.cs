1627e78d3a6820629f67e05e7a999aa04b94cbb5
Bskasan <b.kasan@yandex.com>
2022-11-30 16:25:56 +0200

    Added Raycasts and Debugging to our slime and  Made it switch directions

--- Assets/Scripts/Slime.cs
+++ Assets/Scripts/Slime.cs
@@ -1,10 +1,15 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Slime : MonoBehaviour
 {
+    [SerializeField] private Transform _leftSensor;
+    [SerializeField] private Transform _rightSensor;
+
     private Rigidbody2D _rigidbody2D;
+    private float _direction = -1;
 
     // Start is called before the first frame update
     void Start()
@@ -15,7 +20,34 @@ public class Slime : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        _rigidbody2D.velocity = Vector2.left;
+        var desiredVelocity = new Vector2(_direction, _rigidbody2D.velocity.y);
+        _rigidbody2D.velocity = desiredVelocity;
+        
+       
+        if (_direction < 0)
+        {
+            Debug.DrawRay(_leftSensor.position, Vector2.down * 0.1f, Color.red);
+
+            var result = Physics2D.Raycast(_leftSensor.position, Vector2.down, 0.1f);
+            if (result.collider == null)
+                TurnAround();
+        }
+        else
+        {
+            Debug.DrawRay(_rightSensor.position, Vector2.down * 0.1f, Color.red);
+
+            var result = Physics2D.Raycast(_rightSensor.position, Vector2.down, 0.1f);
+            if (result.collider == null)
+                TurnAround();
+        }
+
+    }
+
+    private void TurnAround()   
+    {
+        _direction *= -1;
+        var spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.flipX = _direction > 0;
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
