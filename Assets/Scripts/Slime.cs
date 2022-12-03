7652e688570dad8c0e3aa4e6a6070f712e8f6429
Bskasan <b.kasan@yandex.com>
2022-12-03 08:23:10 +0200

    Refactored slime sensors and made him detect walls

--- Assets/Scripts/Slime.cs
+++ Assets/Scripts/Slime.cs
@@ -26,21 +26,28 @@ public class Slime : MonoBehaviour
        
         if (_direction < 0)
         {
-            Debug.DrawRay(_leftSensor.position, Vector2.down * 0.1f, Color.red);
-
-            var result = Physics2D.Raycast(_leftSensor.position, Vector2.down, 0.1f);
-            if (result.collider == null)
-                TurnAround();
+            ScanSensor(_leftSensor);
         }
         else
         {
-            Debug.DrawRay(_rightSensor.position, Vector2.down * 0.1f, Color.red);
-
-            var result = Physics2D.Raycast(_rightSensor.position, Vector2.down, 0.1f);
-            if (result.collider == null)
-                TurnAround();
+            ScanSensor(_rightSensor);
         }
 
+    }
+
+    private void ScanSensor(Transform sensor)
+    {
+        Debug.DrawRay(sensor.position, Vector2.down * 0.1f, Color.red);
+        
+
+        var result = Physics2D.Raycast(sensor.position, Vector2.down, 0.1f);
+        if (result.collider == null)
+            TurnAround();
+
+        Debug.DrawRay(sensor.position, new Vector2(_direction, 0) * 0.1f, Color.red);
+        var sideResult = Physics2D.Raycast(sensor.position, new Vector2(_direction, 0), 0.1f);
+        if (sideResult.collider != null)
+            TurnAround();
     }
 
     private void TurnAround()   
