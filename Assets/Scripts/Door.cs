627b436022fc5924df63d38b4da172bb7c947d64
Bskasan <b.kasan@yandex.com>
2022-12-15 11:19:13 +0200

    Working door that opens up

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -12,23 +12,35 @@ public class Door : MonoBehaviour
     [SerializeField] private SpriteRenderer _rendererMid;
     [SerializeField] private int _requiredCoins = 3;
     [SerializeField] private Door _exit;
+    [SerializeField] private Canvas _canvas;
+
+
+    private bool _open;
 
     [ContextMenu("Open Door")] // Great for testing your code/methods.
 
     private void Open() 
     {
+        if (_canvas != null)
+            _canvas.enabled = false;
+
+        _open = true;
         _rendererMid.sprite = _openMid;
         _rendererTop.sprite = _openTop;
     }
 
     void Update()
     {
-        if (Coin.CoinsCollected >= _requiredCoins)
+       
+        if (_open == false && Coin.CoinsCollected >= _requiredCoins)
             Open();
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        if (_open == false)
+            return;
+
         var player = collision.GetComponent<Player>();
         if (player != null && _exit != null) 
         {
