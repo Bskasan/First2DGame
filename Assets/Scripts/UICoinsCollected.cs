0907685a9c0c815bb9df43801d589d3a147ec4d6
Bskasan <b.kasan@yandex.com>
2022-12-23 18:36:06 +0200

    made coinscollected work with tmp

--- Assets/Scripts/UICoinsCollected.cs
+++ Assets/Scripts/UICoinsCollected.cs
@@ -2,18 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class UICoinsCollected : MonoBehaviour
 {
-    private Text _text;
+    private TMP_Text _text;
 
     void Start()
     {
-        _text = GetComponent<Text>();
+        _text = GetComponent<TMP_Text>();
     }
 
     void Update()
     {
-        _text.text = Coin.CoinsCollected.ToString();
+        _text.SetText(Coin.CoinsCollected.ToString());
     }
 }
