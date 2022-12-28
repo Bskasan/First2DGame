0a0b3bdeec136405418d3aff84cf13752672cad7
Bskasan <b.kasan@yandex.com>
2022-12-28 10:53:01 +0200

    Beginning of the wiggle routine

--- Assets/Scripts/FallingPlatform.cs
+++ Assets/Scripts/FallingPlatform.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,6 +6,8 @@ using UnityEngine;
 public class FallingPlatform : MonoBehaviour
 {
     public bool PlayerInside;
+    
+    private HashSet<Player> _playersInTrigger = new HashSet<Player>();
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -12,7 +15,22 @@ public class FallingPlatform : MonoBehaviour
         if (player == null)
             return;
 
+        _playersInTrigger.Add(player); //to add player to the collection
+
         PlayerInside = true;
+
+        StartCoroutine(WiggleAndFall());
+    }
+
+    private IEnumerator WiggleAndFall()
+    {
+        Debug.Log("Waiting to wiggle");
+        yield return new WaitForSeconds(0.25f);
+        Debug.Log("Wiggling");
+        yield return new WaitForSeconds(1f);
+        Debug.Log("Falling");
+        yield return new WaitForSeconds(3f);
+
     }
 
     private void OnTriggerExit2D(Collider2D collision)
@@ -21,6 +39,9 @@ public class FallingPlatform : MonoBehaviour
         if (player == null)
             return;
 
-        PlayerInside = false;
+        _playersInTrigger.Remove(player); // to remove from the collection.
+
+        if(_playersInTrigger.Count == 0)
+            PlayerInside = false;
     }
 }
