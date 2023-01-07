3d567855a2743a1c1d23823de611ec0e7bf5cad2
Bskasan <b.kasan@yandex.com>
2023-01-07 13:56:27 +0200

    Fireball particle system with collision

--- Assets/Scripts/KillOnEnter.cs
+++ Assets/Scripts/KillOnEnter.cs
@@ -11,4 +11,14 @@ public class KillOnEnter : MonoBehaviour
         }
 
     }
+
+    private void OnParticleCollision(GameObject other)
+    {
+        Player player = other.GetComponent<Player>();
+
+        if (player != null)
+        {
+            player.ResetToStart();
+        }
+    }
 }
