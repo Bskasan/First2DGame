b5e2bc11c6d4fd5725619faec68f380dfa8c0244
Bskasan <b.kasan@yandex.com>
2023-01-11 00:51:49 +0200

    Added UI elements to our collector

--- Assets/Scripts/Collector.cs
+++ Assets/Scripts/Collector.cs
@@ -2,32 +2,41 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
+using TMPro;
 
 public class Collector : MonoBehaviour
 {
 
     //Array of Collectibles
     [SerializeField] private Collectible[] _collectibles;
-    
+    private TMP_Text _remainingText;
 
     void Start()
     {
-        
+        _remainingText = GetComponentInChildren<TMP_Text>();
     }
 
     
     void Update()
     {
-
+        int countRemaining = 0;
         // it is better to use this foreach for performance.
         foreach (var collectible in _collectibles) 
         {
             if (collectible.isActiveAndEnabled)
-                return;
+                countRemaining++;
         }
         //for every item in this collection, look at them. check them they're true.
         //if (_collectibles.Any(t => t.gameObject.activeSelf == true))
         //    return;
+
+        //Recommended way
+        _remainingText.SetText(countRemaining.ToString());
+        // Other way;
+        //_remainingText.text = countRemaining.ToString();
+
+        if (countRemaining > 0)
+            return;
                             
         Debug.Log("Got All Gems");
     }
