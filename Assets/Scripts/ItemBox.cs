6b2092ca4ae000b16a0c7eb180be1d86c255f46d
Bskasan <b.kasan@yandex.com>
2023-02-17 21:21:23 +0200

    Added ItemBox

---
+++ Assets/Scripts/ItemBox.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemBox : MonoBehaviour
+{
+    [SerializeField] private Sprite _usedSprite;
+    [SerializeField] private GameObject _item;
+    [SerializeField] private Vector2 _itemLaunchVelocity;
+
+
+    private bool _used;
+
+    private void Start()
+    {
+        if(_item != null)
+           _item.SetActive(false);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (_used)
+            return;
+
+        var player = collision.collider.GetComponent<Player>();
+        if (player == null)
+            return;
+
+        if (collision.contacts[0].normal.y > 0)
+        {  
+            GetComponent<SpriteRenderer>().sprite = _usedSprite;
+            if (_item != null)
+            {
+                _used = true;
+                _item.SetActive(true);
+                var itemRigidbody = _item.GetComponent<Rigidbody2D>();
+                if (itemRigidbody != null) 
+                {
+                    itemRigidbody.velocity = _itemLaunchVelocity;
+                }
+            }
+                
+        }
+
+    }
+}
